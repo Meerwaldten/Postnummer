7cab188ecc7cdda228b0333fa27a573017b98bbc
Meerwaldten <Mikkel_m_j@hotmail.com>
2022-02-22 15:31:57 +0100

    1.0

---
+++ src/Filereader.java
@@ -0,0 +1,32 @@
+import java.io.File;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import static java.lang.Integer.parseInt;
+
+public class Filereader {
+
+
+    public static ArrayList<Postinfo> readFile(){
+        ArrayList<Postinfo> newList = new ArrayList<>();
+        File f = new File("C:\\Users\\mikke\\IdeaProjects\\Postnummer\\src\\Postnumre");
+        try{
+            Scanner sc = new Scanner(f);
+            sc.nextLine();
+            while(sc.hasNext()){
+                String line = sc.nextLine();
+                String[] lineSplit = line.split(":");
+                int postNummer = parseInt(lineSplit[0]);
+                String bynavn = lineSplit[1];
+                Postinfo current = new Postinfo(postNummer, bynavn);
+                newList.add(current);
+            }
+        }catch(Exception e){
+            System.out.println("Timmy er den yngste");
+        }
+        return newList;
+    }
+
+
+}
---
+++ src/Main.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    static ArrayList<Postinfo> listen = Filereader.readFile();
+
+    public static void main(String[] args) {
+
+        System.out.println(skafByNavn(2650));
+        System.out.println(skafByNavn(100000));
+        System.out.println(skafByNavn(8000));
+        System.out.println(skafByNavn(800));
+
+        /*
+        for (Postinfo p: listen) {
+            System.out.println(p);
+        }
+         */
+
+    }
+
+
+    public static String skafByNavn(int postnummer){
+        String bynavn = "By ikke fundet";
+        for (Postinfo p: listen) {
+            if(postnummer==p.getPostNummer()){
+                bynavn = p.getByNavn();
+            }
+        }
+        return bynavn;
+    }
+}
+
+
